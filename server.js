1265962787e7c74e15e6bb11c647d1e453ac2932
Andry J. Hernandez <andryj9622@gmail.com>
2023-05-25 11:18:10 -0300

    M#~:update server handler

--- server.js
+++ server.js
@@ -1,40 +1,39 @@
-const express = require('express');
-const logger = require('morgan');
-
+const express = require("express");
+const logger = require("morgan");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const indexRouter = require('./routes/index');
+const indexRouter = require("./routes/index");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // Not found error handler
 app.use((req, res, next) => {
-    // Create 404 error
-    const error = new Error('Not Found');
-    error.statusCode = 404;
+  // Create 404 error
+  const err = new Error("Not Found");
+  err.statusCode = 404;
 
-    // Pass error to next middleware
-    next(error);
+  // Pass error to next middleware
+  next(err);
 });
 
-
-// Error handler Middleware 
+// Error handler Middleware
 app.use((err, req, res, next) => {
-    // Verify status code
-    const statusCode = err.statusCode || 500;
+  // Verify status code
+  const statusCode = err.statusCode || 500;
 
-    // send response
-    res.status(statusCode).json({
-        status: 'error',
-        message: err.message
-    });
+  // send response
+  res.status(statusCode).json({
+    status: "error",
+    message: "System Error",
+  });
+  console.log(err);
 });
 
 app.listen(port, () => {
-    console.log(`Listening to requests on port ${port}`);
+  console.log(`Listening to requests on port ${port}`);
 });
