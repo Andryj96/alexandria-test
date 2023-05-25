bde82f838d3ab1d4aea631cc77ecd1522a6981f8
Andry J. Hernandez <andryj9622@gmail.com>
2023-05-25 12:19:54 -0300

    F#~:get saved repo by id

--- routes/index.js
+++ routes/index.js
@@ -25,10 +25,22 @@ router.get("/search", async (req, res, next) => {
 });
 
 // List All saved repos without pagination
-router.get("/repos", async (_req, res) => {
+router.get("/repos", async (req, res) => {
   const result = await prisma.repository.findMany();
 
   res.json(result);
+});
+
+// GET details from saved repo by ID
+router.get("/repos/:id", async (req, res) => {
+  const result = await prisma.repository.findUnique({
+    where: { id: parseInt(req.params.id) },
+  });
+
+  if (result === null)
+    res.status(404).json({ message: "Repository not found." });
+    
+  else res.json(result);
 });
 
 module.exports = router;
