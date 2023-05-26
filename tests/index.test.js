b4c0b0c41504acc0d721b8c33dc5f9fab212aefa
Andry J. Hernandez <andryj9622@gmail.com>
2023-05-25 23:18:06 -0300

    M#~:some tests

---
+++ tests/index.test.js
@@ -0,0 +1,17 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("Route tests", () => {
+
+  it("should return a welcome message at the root route", async () => {
+    const response = await request(app).get("/");
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe("Alexandria Tech Teste");
+  });
+
+  it("should return an Array", async () => {
+    const response = await request(app).get("/repos");
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+});
